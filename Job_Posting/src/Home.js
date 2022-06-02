3e6ab02c48d65cdbcf3a0fd88bfa06cf03dd4146
CrazyJay10 <101778859+CrazyJay10@users.noreply.github.com>
2022-06-02 23:09:16 +0800

    Add files via upload

---
+++ Job_Posting/src/Home.js
@@ -0,0 +1,6 @@
+
+const Home = () => {
+    return <h1 style={{ textAlign: "center" }}>Home page</h1>;
+}
+ 
+export default Home;
---
+++ Job_Posting/src/Navbar.js
@@ -0,0 +1,23 @@
+const { Link } = require("react-router-dom");
+const Navbar = () => {
+ return (
+ <nav style={{ textAlign: "center", marginTop: "20px" }}>
+ <Link to="/" style={{ padding: "10px" }}>
+ Home
+ </Link>
+ <Link to="/profile" style={{ padding: "10px" }}>
+ Profile
+ </Link>
+ <Link to="/about" style={{ padding: "10px" }}>
+ About
+ <Link to="/" style={{ padding: "10px" }}>
+ Jobs
+ </Link>
+ </Link>
+ <Link to="/login" style={{ padding: "40px" }}>
+ Login
+ </Link>
+ </nav>
+ );
+};
+export default Navbar;
